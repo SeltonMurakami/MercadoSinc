1bb024f53a655ac358f0109217c9414a19e12375
Selton Naoki Murakami <33372236+SeltonMurakami@users.noreply.github.com>
2021-01-13 23:19:01 -0300

    Caso Especifico
    Adiciona copia de perfil
    Refinado para os anuncios da conta Murakami Ferramentas

--- copia.py
+++ copia.py
@@ -4,21 +4,27 @@ import pickle
 
 tokens = pickle.load(open('keys.pkl', 'rb'))
 
-def copia(nml, qtd, pre, token, datapath = None, ean = None):
+def copia(nml, qtd, pre, token, datapath = None, ean = None, rp = None):
 	headers = {"Authorization":"Bearer "+token}
 	r = requests.get("https://api.mercadolibre.com/items/MLB"+nml, headers = headers)
 	if r.status_code != 200:
 		return r
 	base = json.loads(r.text)
-	desc = json.loads(requests.get("https://api.mercadolibre.com/items/MLB"+nml+"/descriptions", headers = headers).text)[0]
+	if base['status'] != 'active':
+		return
+	desc = json.loads(requests.get("https://api.mercadolibre.com/items/MLB"+nml+"/descriptions",  headers = headers).text)[0]
 	dele(desc, 'id', 'created')
 	print(desc['text'])
 	print(desc['plain_text'])
-	if input("editar desc?") == 's':
-		desc['plain_text'] == input("Descrição: ")
 	dele(base,'expiration_time', 'item_relations', 'geolocation', 'end_time', 'inventory_id', 'stop_time', 'start_time', 'id', 'site_id', 'seller_id', 'sold_quantity', 'initial_quantity', 'original_price', 'base_price', "international_delivery_mode", "subtitle", "permalink", "thumbnail_id", "thumbnail", "secure_thumbnail", "descriptions", "seller_address", "seller_contact", "location", "coverage_areas", "warnings", "listing_source", "status", "sub_status", "tags", "catalog_product_id", "domain_id", "parent_item_id", "differential_pricing", "deal_ids", "date_created", "last_updated", "health", "catalog_listing", )
 	base['description'] = {"plain_text": desc['plain_text']}
-	base['price'] = pre
+	if rp:
+		base['price'] = rp(base['price'])
+	else:
+		base['price'] = pre
+	for n, i in enumerate(base['attributes']):
+		if i['id'] in ["PACKAGE_HEIGHT", "FREQUENCY", "SHIPMENT_PACKING", "PACKAGE_WIDTH", "PACKAGE_WEIGHT", "PACKAGE_LENGTH"]:
+			del base['attributes'][n]
 	headers["Content-Type"] = "application/json"
 	r = requests.post("https://api.mercadolibre.com/items", data = json.dumps(base), headers = headers)
 	print(r.text)
@@ -28,10 +34,18 @@ def copia(nml, qtd, pre, token, datapath = None, ean = None):
 			d = pickle.load(open(datapath, 'rb'))
 			d[ean] = [json.loads(r.text)['title'], json.loads(r.text)['id'][3:]]
 			pickle.dump(d, open(datapath, 'wb'))
+	#elif r.status_code == 400:
+	#	response = json.loads(r.text)
+	#	if response['error'] == 'validation_error':
+	#		delatt = []
+	#		for i in response['cause']:
+	#			st = i['message']
+	#			att = st[st.index('[')+1:st.index(']')]
+	#			delatt.append(att)
 
-def busca(nome, ean, token):
+def busca(query, ean, token, arg = 'q'):
 	headers = {"Authorization":"Bearer "+token}
-	r = requests.get("https://api.mercadolibre.com/sites/MLB/search?q="+nome.replace(" ","%20"), headers = headers)
+	r = requests.get("https://api.mercadolibre.com/sites/MLB/search?"+arg+"="+query.replace(" ","%20"), headers = headers)
 	ids = [x['id'] for x in json.loads(r.text)['results']]
 	print(nome)
 	for i in ids:
@@ -48,6 +62,22 @@ def busca(nome, ean, token):
 		print(resp['title'])
 		if input('seleciona? ') == 's':
 			return i
+
+def altbusca(seller_id, token):
+	headers = {"Authorization":"Bearer "+token}
+	r = requests.get("https://api.mercadolibre.com/sites/MLB/search?seller_id="+seller_id, headers = headers)
+	response = json.loads(r.text)
+	lista = [x['id'] for x in response['results']]
+	total = response['paging']['total']
+	for i in range(50, total, 50):
+		try:
+			r = requests.get("https://api.mercadolibre.com/sites/MLB/search?offset="+str(i)+"&seller_id="+seller_id, headers = headers)
+			response = json.loads(r.text)
+			lista += [x['id'] for x in response['results']]
+		except Exception as e:
+			print(e)
+			print(i)
+	return lista
 
 def dele(item, *args):
 	for i in args:
--- get_token.py
+++ get_token.py
@@ -18,7 +18,7 @@ with meli.ApiClient() as api_client:
     client_id = '6545766642471155' # Your client_id
     client_secret = 'TnkpxGW9LnCbaYrnGvetdZ2lfj3udxjE' # Your client_secret
     redirect_uri = 'http://localhost:3000' # Your redirect_uri
-    code = 'TG-5ff895e87b919100066e756a-657456460' # The parameter CODE
+    code = 'TG-5fff8651e1c95500078028ad-296166111' # The parameter CODE
     refresh_token = 'refresh_token_example' # Your refresh_token
 
 try:
